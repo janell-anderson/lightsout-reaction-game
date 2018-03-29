395f932b362e92b2446ca46f24ffa2c0ea82280c
Janell Anderson <mouse@ool-ad0201ec.dyn.optonline.net>
2018-03-29 19:01:04 -0400

    starting setInterval function

--- src/app.js
+++ src/app.js
@@ -13,41 +13,30 @@ $('.lightsOn').hide();
 
 // create play button on index.html page
 // When you press play the sequence will start
-const lights = $('#play');
+const lights = [];
   $('#play').on('click', function() {
-    $('.lightsOn').hide().delay(1000).fadeIn(2000)
+    $('.lightsOn').delay(1000).fadeIn(2000)
 
-// after sequence has faded in it will randomly fade out
-for (let i = 0;  i < lights.length; i++) {
-    (function(i) {
-        setTimeout(function () {
-            console.log(i);
-        }, Math.floor(Math.random() * 1000));
-    })(i);
-}
+// after sequence has faded in it will then fade out at random instances
+
+// setInterval()
 
 });
 
 // Create event button so player can get time
+$( ".lightsOn" ).click(function() {
+  $( ".lights" ).mousedown().delay(1000).fadeIn(2000);
+});
 
+setInterval(function(){ alert("Hello"); }, 3000);
+
+// addEventListener('mousedown', event => {
+//   if (event.button === 0) tap(event);
+// }, {passive: false});
 
 
 // create reset button on index.html page
   $('#reset').click(function() {
     window.location.href = 'index.html';
 });
-
-
-// GAME LOGIC
-/*
-CREATE EMPTY ARRAY FOR STORED TIMES
-
-*/
-});
-
-let timer = setInterval(changeLights, 3000);
-let index = 0;
-function changeLights(){
-  index = index + 1;
-
-}
+  });
