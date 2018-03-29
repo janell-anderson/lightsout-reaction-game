f0ab5d3e7a180f99d0fdd85f4e4880f90c39ff66
Janell Anderson <mouse@ool-ad0201ec.dyn.optonline.net>
2018-03-29 16:50:57 -0400

    jQuery play button works with css animations

--- src/app.js
+++ src/app.js
@@ -1,26 +1,40 @@
+  /*eslint-env jquery*/
+
 $(document).ready(function () {
 
 
-
-// create play button on index.html page
-  let playbtn = "#play";
-  $('#play').click(function() {
-    alert('Hey I WORK!')
-    // $('.lightsOn').click();
-});
-
+$('.lightsOn').hide();
 
 // create enter button for landing page
-  let enterbtn = "#enter";
+  const enterbtn = ('#enter');
   $('enter').click(function() {
     window.location.href = 'index.html';
 });
 
+// create play button on index.html page
+// When you press play the sequence will start
+const lights = $('#play');
+  $('#play').on('click', function() {
+    $('.lightsOn').hide().delay(1000).fadeIn(2000)
+
+// after sequence has faded in it will randomly fade out
+for (let i = 0;  i < lights.length; i++) {
+    (function(i) {
+        setTimeout(function () {
+            console.log(i);
+        }, Math.floor(Math.random() * 1000));
+    })(i);
+}
+
+});
+
+// Create event button so player can get time
+
+
+
 // create reset button on index.html page
-  let resetbtn = "#reset";
   $('#reset').click(function() {
-    alert('Hey I WORK!')
-    // window.location.href = 'index.html';
+    window.location.href = 'index.html';
 });
 
 
@@ -29,8 +43,11 @@ $(document).ready(function () {
 CREATE EMPTY ARRAY FOR STORED TIMES
 
 */
-
-
-
-
 });
+
+let timer = setInterval(changeLights, 3000);
+let index = 0;
+function changeLights(){
+  index = index + 1;
+
+}
