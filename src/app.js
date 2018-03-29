59ed082e37ff0fe65dceee5bc157c2cd95a5e109
Janell Anderson <mouse@Janells-MacBook-Pro.local>
2018-03-28 20:00:03 -0400

    worked on lights and button to begin on game logic

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
 $(document).ready(function () {
+
+
+
+// create play button on index.html page
+  let playbtn = "#play";
+  $('#play').click(function() {
+    alert('Hey I WORK!')
+    // $('.lightsOn').click();
+});
+
+
 // create enter button for landing page
   let enterbtn = "#enter";
   $('enter').click(function() {
     window.location.href = 'index.html';
 });
 
-// create play button on index.html page
-  let playbtn = "#play";
-  $('play').click(function() {
-    window.location.href = 'index.html';
-});
-
 // create reset button on index.html page
   let resetbtn = "#reset";
-  $('reset').click(function() {
-    window.location.href = 'index.html';
+  $('#reset').click(function() {
+    alert('Hey I WORK!')
+    // window.location.href = 'index.html';
 });
-
 
 
 // GAME LOGIC
