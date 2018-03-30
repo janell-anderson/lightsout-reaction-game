367828ce8d8a26180dd548a1239e041a82d86d57
Janell Anderson <mouse@Janells-MBP.home>
2018-03-30 02:27:34 -0400

    trying out setInterval fadeOut's part II

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@
 
 $(document).ready(function () {
 
+let lightsOn = $('.lightsOn');
+let lights =   $( ".lights" );
+let time = 35;
 
 $('.lightsOn').hide();
 
@@ -13,22 +16,29 @@ $('.lightsOn').hide();
 
 // create play button on index.html page
 // When you press play the sequence will start
-const lights = [];
   $('#play').on('click', function() {
     $('.lightsOn').delay(1000).fadeIn(2000)
+});
 
 // after sequence has faded in it will then fade out at random instances
-
-// setInterval()
-
-});
+function play() {
+  time = setInterval(function(){
+    $(lightsOn);
+    if (sec === -2) {
+      $(lightsOn).fadeOut('fast');
+    }
+  }, 3000);
+}
+//  setInterval(function(){
+//   $( ".lightsOn" ).fadeOut('slow');
+// }, 3000);
 
 // Create event button so player can get time
-$( ".lightsOn" ).click(function() {
-  $( ".lights" ).mousedown().delay(1000).fadeIn(2000);
-});
 
-setInterval(function(){ alert("Hello"); }, 3000);
+
+// setInterval(function(){
+//   alert("Hello");
+// }, 3000);
 
 // addEventListener('mousedown', event => {
 //   if (event.button === 0) tap(event);
