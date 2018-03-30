b03a2937a20118c723f22a5383a510007e3125de
Janell Anderson <mouse@ool-ad0201ec.dyn.optonline.net>
2018-03-30 11:00:25 -0400

    figured out the delay for lights to turn off. Will do random delay later. Working on creating a eventListener

--- src/app.js
+++ src/app.js
@@ -4,41 +4,36 @@ $(document).ready(function () {
 
 let lightsOn = $('.lightsOn');
 let lights =   $( ".lights" );
-let time = 35;
+let speed = [];
+let time;
 
+// Hides the lights on the initial page until they press "PLAY"
+// Did not use the enter button since the landing page will be on the same html file
 $('.lightsOn').hide();
 
 // create enter button for landing page
-  const enterbtn = ('#enter');
-  $('enter').click(function() {
-    window.location.href = 'index.html';
-});
+//   const enterbtn = ('#enter');
+//   $('enter').click(function() {
+//     window.location.href = 'index.html';
+// });
 
 // create play button on index.html page
 // When you press play the sequence will start
-  $('#play').on('click', function() {
-    $('.lightsOn').delay(1000).fadeIn(2000)
-});
-
 // after sequence has faded in it will then fade out at random instances
-function play() {
-  time = setInterval(function(){
-    $(lightsOn);
-    if (sec === -2) {
-      $(lightsOn).fadeOut('fast');
-    }
-  }, 3000);
+function play () {
+
+  $('#play').on('click', function() {
+ ($('.lightsOn').delay(1000).fadeIn(2000).delay(10000).fadeOut(1000));
+
+ // randomized delay
+ // (Math.floor(Math.random() * 10000))
+});
 }
-//  setInterval(function(){
-//   $( ".lightsOn" ).fadeOut('slow');
-// }, 3000);
+  play();
 
 // Create event button so player can get time
-
-
-// setInterval(function(){
-//   alert("Hello");
-// }, 3000);
+// When user clicks on the screen they will get there speed
+//
 
 // addEventListener('mousedown', event => {
 //   if (event.button === 0) tap(event);
