a786ccaa075ff9ff34dae6b21025ad0d4c4ece88
Janell Anderson <mouse@ool-ad0201ec.dyn.optonline.net>
2018-03-30 11:57:13 -0400

    working on time and eventListener

--- src/app.js
+++ src/app.js
@@ -20,24 +20,37 @@ $('.lightsOn').hide();
 // create play button on index.html page
 // When you press play the sequence will start
 // after sequence has faded in it will then fade out at random instances
+// remember to put e into the function();
+
+// Create event button so player can get time
+// When user clicks on the screen they will get there speed
+// Instead of function addTime immeaditely appearing in console.log
+// I want it to fade out and then have a mouse event
+// to initiate function addTime to grab the milliseconds
+// and place it into the global array called speed
+
+  let addTime = function() {;
+    let d = Date.now();
+    console.log(d);
+    // e.stopPropagation();
+    speed.push(d);
+    console.log(speed);
+}
+
 function play () {
 
   $('#play').on('click', function() {
- ($('.lightsOn').delay(1000).fadeIn(2000).delay(10000).fadeOut(1000));
+ ($('.lightsOn').delay(1000).fadeIn(2000).delay(10000).fadeOut(500, addTime))
+
 
  // randomized delay
+ // Math.ceil(Math.random() * 10000)
  // (Math.floor(Math.random() * 10000))
+ // Math.random() * 4000 + 1000
 });
 }
   play();
 
-// Create event button so player can get time
-// When user clicks on the screen they will get there speed
-//
-
-// addEventListener('mousedown', event => {
-//   if (event.button === 0) tap(event);
-// }, {passive: false});
 
 
 // create reset button on index.html page
